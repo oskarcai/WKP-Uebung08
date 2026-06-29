176e1867632698e2fc3c7c7a2a6b3487fe56372c
oskarcai <oskarcaicai@gmail.com>
2026-06-29 12:30:30 +0200

    .

--- src/A1_Generische_Stapelklasse/ZahlStapel.java
+++ src/A1_Generische_Stapelklasse/ZahlStapel.java
@@ -5,7 +5,7 @@ public class ZahlStapel<T extends Number> extends Stapel<T> {
     // Implementieren Sie die Methode double summe(): Alle Elemente in elemente werden
     // durchlaufen und deren Summe wird als double zurückgegeben.
     double summe() {
-        double sum = 0;
+        double sum = 0.0;
         for(T number : elemente) {
             sum += number.doubleValue();
         }
@@ -15,7 +15,7 @@ public class ZahlStapel<T extends Number> extends Stapel<T> {
     // Implementieren Sie die Methode double durchschnitt(): Gibt den Durchschnittswert
     // aller Elemente zurück. Sie dürfen davon ausgehen, dass der Stapel nicht leer ist.
     double durchschnitt() {
-        return summe() / elemente.size();
+        return summe() / groesse();
     }
 
 
